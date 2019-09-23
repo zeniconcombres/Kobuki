13a0690c19e7b82de286e302374d9cd91291e582
Inex <Re>
2019-09-23 22:39:24 +1000

    Completed the statechart with L and R rotations with hillClimb component.

--- src/C Statechart/KobukiNavigationStateChart.c
+++ src/C Statechart/KobukiNavigationStateChart.c
@@ -10,15 +10,19 @@
 
 // Program States
 typedef enum{
-    INITIAL = 0,                        // Initial state
-    PAUSE_WAIT_BUTTON_RELEASE,            // Paused; pause button pressed down, wait until released before detecting next press
-    UNPAUSE_WAIT_BUTTON_PRESS,            // Paused; wait for pause button to be pressed
-    UNPAUSE_WAIT_BUTTON_RELEASE,        // Paused; pause button pressed down, wait until released before returning to previous state
-    DRIVE,                                // Drive straight
-    REVERSE,                            // Reverse straight
-    TURN_RIGHT,                                // Turn right
+    INITIAL = 0,                            // Initial state
+    PAUSE_WAIT_BUTTON_RELEASE,              // Paused; pause button pressed down, wait until released before detecting next press
+    UNPAUSE_WAIT_BUTTON_PRESS,              // Paused; wait for pause button to be pressed
+    UNPAUSE_WAIT_BUTTON_RELEASE,            // Paused; pause button pressed down, wait until released before returning to previous state
+    DRIVE,                                  // Drive straight
+    REVERSE,                                // Reverse straight
+    TURN_RIGHT,                             // Turn right
     TURN_LEFT,                              // Turn left
-    DRIVE_AVOID                             // Drive after reverse and turning from object
+    DRIVE_AVOID,                            // Drive after reverse and turning from object
+    ROTATE_LEFT,                            // Rotating machine left to directly climb
+    ROTATE_RIGHT,                           // Rotating machine right to directly climb
+    TURNAROUND,                             // Turn 180 degrees after hitting top of ramp
+    STOP                                    // Robot stops after descending hill
 } robotState_t;
 
 #define DEG_PER_RAD            (180.0 / M_PI)        // degrees per radian
@@ -28,23 +32,23 @@ void KobukiNavigationStatechart(
                                 const int16_t                 maxWheelSpeed,
                                 const int32_t                 netDistance,
                                 const int32_t                 netAngle,
-                                const KobukiSensors_t        sensors,
-                                const accelerometer_t        accelAxes,
-                                int16_t * const             pRightWheelSpeed,
-                                int16_t * const             pLeftWheelSpeed,
+                                const KobukiSensors_t         sensors,
+                                const accelerometer_t         accelAxes,
+                                int16_t * const               pRightWheelSpeed,
+                                int16_t * const               pLeftWheelSpeed,
                                 const bool                    isSimulator
                                 ){
     
     // local state
-    static robotState_t         state = INITIAL;                // current program state
-    static robotState_t            unpausedState = DRIVE;            // state history for pause region
+    static robotState_t           state = INITIAL;                // current program state
+    static robotState_t           unpausedState = DRIVE;            // state history for pause region
     static int32_t                distanceAtManeuverStart = 0;    // distance robot had travelled when a maneuver begins, in mm
     static int32_t                angleAtManeuverStart = 0;        // angle through which the robot had turned when a maneuver begins, in deg
-    static int32_t              acceleromterZ = 0;       // accelerometer reading for Z at the very
+    static int32_t                acceleromterZ = 0;       // accelerometer reading for Z at the very
     
     // outputs
-    int16_t                        leftWheelSpeed = 0;                // speed of the left wheel, in mm/s
-    int16_t                        rightWheelSpeed = 0;            // speed of the right wheel, in mm/s
+    int16_t                       leftWheelSpeed = 0;                // speed of the left wheel, in mm/s
+    int16_t                       rightWheelSpeed = 0;            // speed of the right wheel, in mm/s
     
     // speed constants
     int16_t                     speed = 100;                    // overall running speed of the robot, in mm/s
@@ -53,8 +57,8 @@ void KobukiNavigationStatechart(
     static int16_t              right = 0;                      // any right sensor is triggered in obstacle avoidance
     static int16_t              left = 0;                       // any left sensor is triggered in obstacle avoidance
     static int16_t              central = 0;                    // any central sensor is triggered in obstacle avoidance
-    static int16_t              hill = 0;                       // to denote if robot is on a hill climb
-    static int16_t              nohill = 0;                     // to denote if robot is not on a hill climb
+    static int16_t              hillClimb = 0;                       // to denote if robot is on a hill climb
+    static int16_t              flat = 0;                     // to denote if robot is not on a hill climb
     static int16_t              avoided = 0;                    // registering obstacle avoidance
     
     // sensor thresholds
@@ -64,7 +68,10 @@ void KobukiNavigationStatechart(
     // state data - process inputs                        *
     //*****************************************************
     
-    // maybe we should initialise the accelerometer Z axis readings at the start?
+    // ***** NOTES from code: *****
+    // maybe we should initialise the accelerometer Z axis readings at the start? - kind of have done this
+    // could consider fine tuning the side (R/L) sensors because they never seem to be benefitting from a 90 degrees turn
+    // for object avoidance? especially if there are things on the hill
     
     if (state == INITIAL
         || state == PAUSE_WAIT_BUTTON_RELEASE
@@ -153,13 +160,18 @@ void KobukiNavigationStatechart(
             case REVERSE:
                 // move on to turning
                 if (abs(netDistance - distanceAtManeuverStart) >= 100){
-                    if (central || left){
-                        // default is to turn right
-                        state = TURN_RIGHT;
-                    }
-                    else {
-                        // left turn
-                        state = TURN_LEFT;
+                    if (hillClimb && flat) {
+                        // after hitting the wall at the top of the hill
+                        state = TURNAROUND;
+                    } else {
+                        if (central || left){
+                            // default is to turn right
+                            state = TURN_RIGHT;
+                        }
+                        else {
+                            // left turn
+                            state = TURN_LEFT;
+                        }
                     }
                     angleAtManeuverStart = netAngle;
                     distanceAtManeuverStart = netDistance;
@@ -196,29 +208,82 @@ void KobukiNavigationStatechart(
                     distanceAtManeuverStart = netDistance;
                 }
                 break;
+            default:
+                angleAtManeuverStart = netAngle;
+                distanceAtManeuverStart = netDistance;
+                state = DRIVE;
+                break;
         }
     }
     // else, no transitions are taken and the robot stays in DRIVE
     //*************************************
     // state transition - hill climb region      *
     //*************************************
-    else if (abs(accelAxes.z - accelerometerZ) >= sensorDiff*accelerometerZ) {
+    else if (state = ROTATE_LEFT
+             || state = ROTATE_RIGHT
+             || state = TURNAROUND
+             || (abs(accelAxes.z - accelerometerZ) >= sensorDiff*accelerometerZ)
+             || abs(accelAxes.x) >= sensorDiff) {
         // branch with pause states and obstacle avoidance states
-        switch (hill) {
+        switch (hillClimb) {
             case 0:
                 // basically change from flat ground to register hill climb
-                nohill = 0;
-                hill = 1;
-            case 1:
-                // on the hill transition to hill top or flat ground or hill top
-                if (accelAxes.x < 0) {
-                    // is this going down??
-                } else if (accelAxes.x > 0) {
-                    // is this going up??
-                } else if (abs(accelAxes.z - accelerometerZ) <= sensorDiff*accelerometerZ) {
-                    // when we reach hill top the next thing we bump will be the wall - turn!
-                    
+                flat = 0;
+                hillClimb = 1;
+                if (accelAxes.y >= sensorDiff) {
+                    // robot needs to turn right to correct left slant
+                    state = ROTATE_RIGHT;
+                } else if (accelAxes.y <= -sensorDiff) {
+                    // robot needs to turn left to correct right slant
+                    state = ROTATE_LEFT;
                 }
+                break;
+            case 1:
+                // on the hill transition to hill top or hill top to coming down
+                if (flat == 1 && accelAxes.x <= sensorDiff) {
+                    // going down
+                    flat = 0;
+                    hillClimb = -1;
+                    if (accelAxes.y >= sensorDiff) {
+                        // robot needs to turn left to correct right down slant
+                        state = ROTATE_LEFT;
+                    } else if (accelAxes.y <= -sensorDiff) {
+                        // robot needs to turn right to correct left down slant
+                        state = ROTATE_RIGHT;
+                    }
+                } else if (abs(accelAxes.z - accelerometerZ) <= sensorDiff*accelerometerZ
+                           && abs(accelAxes.x) <= sensorDiff) {
+                    // when we reach hill top the next thing we bump will be the wall - turn!
+                    flat = 1;
+                }
+                break;
+            case -1:
+                // robot stops after reaching ground from hill descent
+                if (abs(accelAxes.z - accelerometerZ) <= sensorDiff*accelerometerZ
+                    && abs(accelAxes.x) <= sensorDiff) {
+                    flat = 1;
+                    hillClimb = 0;
+                    state = STOP;
+                }
+                break;
+        }
+        switch (state) {
+            case ROTATE_LEFT:
+            case ROTATE_RIGHT:
+                if (abs(accelAxes.y) <= sensorDiff) {
+                    angleAtManeuverStart = netAngle;
+                    distanceAtManeuverStart = netDistance;
+                    state = DRIVE;
+                } // otherwise remain rotating until fixed
+                break;
+            case TURNAROUND:
+                // turning around completely after hitting wall keeping under 180 degrees turn
+                if (abs(netAngle - angleAtManeuverStart) >= 179) {
+                    angleAtManeuverStart = netAngle;
+                    distanceAtManeuverStart = netDistance;
+                    state = DRIVE;
+                } // otherwise remain rotating until fixed
+                break;
         }
     }
     
@@ -230,6 +295,7 @@ void KobukiNavigationStatechart(
         case PAUSE_WAIT_BUTTON_RELEASE:
         case UNPAUSE_WAIT_BUTTON_PRESS:
         case UNPAUSE_WAIT_BUTTON_RELEASE:
+        case STOP:
             // in pause mode, robot should be stopped
             leftWheelSpeed = rightWheelSpeed = 0;
             break;
@@ -241,11 +307,14 @@ void KobukiNavigationStatechart(
             break;
             
         case TURN_RIGHT:
+        case ROTATE_RIGHT:
+        case TURNAROUND:
             leftWheelSpeed = speed;
             rightWheelSpeed = -leftWheelSpeed;
             break;
             
         case TURN_LEFT:
+        case ROTATE_LEFT:
             rightWheelSpeed = speed;
             leftWheelSpeed = -rightWheelSpeed;
             break;
